af5352e1d4912c6a177326f7879453ae16828e49
Alaude30 <kuramammode11@gmail.com>
2019-03-07 16:05:37 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-test';
+  load() {
+    const script = document.createElement('script');
+    script.src = 'assets/el.js';
+    document.body.appendChild(script);
+    const el = document.createElement('app-live-chat');
+    el.setAttribute('domain', '1234');
+    el.setAttribute('type', 'host');
+    el.setAttribute(
+      'style',
+      `position: fixed; z-index: 123; bottom: 1em; right: 1em; display: block; border: 2px solid grey; width: 340px; height: 480px;`
+    );
+    document.querySelector('#container').appendChild(el);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
